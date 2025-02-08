82219bd37faffdaea100bacb291127e1a26ff8bf
hodgesd <hodgesd@gmail.com>
2025-02-08 14:45:15 -0600

    Remove initial Django project files and configuration
    Completely deleted the base setup of a Django project, including the `leads_app` application and its associated files. This includes configuration, templates, styles, and static assets, effectively resetting the project structure.

--- config/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-from leads_app import views
-
-urlpatterns = [
-    path("", include("leads_app.urls")),
-    path("admin/", admin.site.urls),
-    
-]
--- config/leads_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from leads_app import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('submit-lead/', views.submit_lead, name='submit_lead'),
-]
